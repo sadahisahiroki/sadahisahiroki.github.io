95a83fb8846e9806bbdaa9100105ff7db90801de
hiroki_sadahisa <sadahisa@kairos.co.jp>
2017-12-29 16:59:53 +0900

    いったん終了

--- source/js/common.js
+++ source/js/common.js
@@ -2,74 +2,75 @@ const browserStateChange = require("./utils/browserStateChange");
 
 AOS.init({
   duration: 800,
-  easing: "ease-out-quad"
+  easing: "ease-out-quad",
+  once: true
 });
 
-// /**
-// * three.js
-// *
-// */
-// let container;
-// let camera, scene, renderer;
-// let uniforms;
+/**
+* three.js
+*
+*/
+let container;
+let camera, scene, renderer;
+let uniforms;
 
-// const w = window.innerWidth;
-// const h = window.innerHeight;
+const w = window.innerWidth;
+const h = window.innerHeight;
 
 
-// init();
-// animate();
+init();
+animate();
 
-// function init() {
-//   container = document.getElementById("canvas");
+function init() {
+  container = document.getElementById("canvas");
 
-//   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
-//   camera.position.z = 100;
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 100;
 
-//   scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-//   const geometry = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight, 1);
+  const geometry = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight, 1);
 
-//   uniforms = {
-//     time: {value: 0.0},
-//     resolution: {value: new THREE.Vector2()},
-//   };
-//   const material = new THREE.ShaderMaterial({
-//     uniforms: uniforms,
-//     vertexShader: document.getElementById( 'vertexShader' ).textContent,
-//     fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-//   });
+  uniforms = {
+    time: {value: 0.0},
+    resolution: {value: new THREE.Vector2()},
+  };
+  const material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById( 'vertexShader' ).textContent,
+    fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+  });
 
-//   const mesh = new THREE.Mesh(geometry, material);
+  const mesh = new THREE.Mesh(geometry, material);
 
-//   scene.add(mesh);
+  scene.add(mesh);
 
-//   renderer = new THREE.WebGLRenderer();
-//   renderer.setPixelRatio(window.devicePixelRatio);
-//   container.appendChild(renderer.domElement);
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  container.appendChild(renderer.domElement);
 
-//   onWindowResize();
-//   browserStateChange.resize(onWindowResize);
+  onWindowResize();
+  browserStateChange.resize(onWindowResize);
 
-// }
+}
 
-// function onWindowResize(e) {
+function onWindowResize(e) {
 
-//   uniforms.resolution.value.x = w;
-//   uniforms.resolution.value.y = h;
+  uniforms.resolution.value.x = w;
+  uniforms.resolution.value.y = h;
 
-//   camera.aspect = w / h;
-//   camera.updateProjectionMatrix();
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
 
-//   renderer.setSize(w, h);
-// }
+  renderer.setSize(w, h);
+}
 
-// function animate() {
-//   requestAnimationFrame(animate);
-//   render();
-// }
+function animate() {
+  requestAnimationFrame(animate);
+  render();
+}
 
-// function render() {
-//   uniforms.time.value += 0.01;
-//   renderer.render(scene, camera) 
-// }
+function render() {
+  uniforms.time.value += 0.01;
+  renderer.render(scene, camera) 
+}
